47897fd6c507981e37f4a2f01d2a2016b8ba16f4
Arup-04 <160034173+Arup-04@users.noreply.github.com>
2024-08-31 20:27:32 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7377106068:AAHrsLBCzA3wzLadWF1M-iIUbJFhQt3jQOk")
     
     API_ID = int(os.environ.get("API_ID", "24828869"))
     
